d4540cadb1b6c26bfa76d969423c9b6ecc349b53
Lucas Takeshi <takeshioficial01@gmail.com>
2024-10-11 21:42:08 -0300

    feat: adicionando o mariadb:10.11

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
+$pdo = null;
+try {
+    $pdo = new PDO('mysql:host=mysql;dbname=database', 'root', 'root');
+} catch (PDOException $e) {
+    print $e->getMessage();
+    die();
+}
 
 phpinfo();
-
-?>
