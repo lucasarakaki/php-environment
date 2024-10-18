ce958616f3eecbf27601be276f0a1e02daf6e7f1
Lucas Takeshi <takeshioficial01@gmail.com>
2024-10-18 14:58:54 -0300

    fix(db): changing database connection

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 $pdo = null;
 try {
-    $pdo = new PDO('mysql:host=mysql;dbname=database', 'root', 'root');
+    $pdo = new PDO('mysql:host=mariadb_dkr;dbname=database', 'root', 'root');
 } catch (PDOException $e) {
     print $e->getMessage();
     die();
